5f08328e87e079ea204edc248c87ec00e1082f25
MarcArmbruster_cp <SND\MarcArmbruster_cp>
2014-12-23 15:46:16 +0000

    Initial Version

---
+++ CommandAggregator/CmdAgg/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommandAggregatorExample.ViewModels;
+using System.Windows;
+
+namespace CommandAggregatorExample
+{
+    /// <summary>
+    /// The Application Entry Point.
+    /// </summary>   
+    /// <remarks>
+    ///   <para><b>History</b></para>
+    ///   <list type="table">
+    ///   <item>
+    ///   <term><b>Author:</b></term>
+    ///   <description>Marc Armbruster</description>
+    ///   </item>
+    ///   <item>
+    ///   <term><b>Date:</b></term>
+    ///   <description>Dec/23/2014</description>
+    ///   </item>
+    ///   <item>
+    ///   <term><b>Remarks:</b></term>
+    ///   <description>Initial version.</description>
+    ///   </item>
+    ///   </list>
+    /// </remarks>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Definition of the starting view incl. the view model.
+        /// </summary>
+        /// <param name="e">The startup event arguments.</param>       
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow { DataContext = new MainVm() };
+            window.Show();
+        }
+    }
+}
---
+++ CommandAggregator/CmdAgg/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("WPF Command Aggregator")]
+[assembly: AssemblyDescription("WPF Command Aggregator")]
+[assembly: AssemblyConfiguration(".net 4.0")]
+[assembly: AssemblyCompany("Marc Armbruster")]
+[assembly: AssemblyProduct("WPF Command Aggregator")]
+[assembly: AssemblyCopyright("Copyright © Marc Armbruster 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
