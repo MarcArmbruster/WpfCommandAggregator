01b6ea0c1b567c70f2ad44a21bb6823772d2a08a
MarcArmbruster_cp <SND\MarcArmbruster_cp>
2015-08-15 12:26:29 +0000

    Unit Tests

--- CommandAggregator/UnitTests/HierarchyCommandTests.cs
+++ CommandAggregator/UnitTests/HierarchyCommandTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
-using WPFCommandAggregator.Implementation;
+using WPFCommandAggregator;
 
 namespace UnitTests
 {
