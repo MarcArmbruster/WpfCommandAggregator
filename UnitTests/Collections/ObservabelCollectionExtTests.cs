0e702e501078d6556c3ae0748f01cfb846d5762b
Marc Armbruster <marcarmbruster@hotmail.de>
2019-12-14 16:41:34 +0100

    ObservabelCollectionExt

---
+++ UnitTests/Collections/ObservabelCollectionExtTests.cs
@@ -0,0 +1,83 @@
+﻿namespace UnitTests.Collections
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// Summary description for ObservabelCollectionExtTests
+    /// </summary>
+    [TestClass]
+    public class ObservabelCollectionExtTests
+    {        
+        private class TestClass
+        {
+            public int Id { get; set; }
+            public string Value { get; set; }
+        }
+
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            ObservableCollectionExt<TestClass> collection = new ObservableCollectionExt<TestClass>();
+
+            var obj1 = new TestClass { Id = 1, Value = "one" };
+            var obj2 = new TestClass { Id = 2, Value = "two" };
+            var obj3 = new TestClass { Id = 3, Value = "three" };
+            var obj4 = new TestClass { Id = 4, Value = "four" };
+
+            collection.Add(obj1);
+            collection.Add(obj2);
+            collection.Add(obj3);
+
+            collection.Replace(obj2, obj4);
+
+            Assert.AreEqual(3, collection.Count);
+            Assert.AreEqual(1, collection[0].Id);
+            Assert.AreEqual(4, collection[1].Id);
+            Assert.AreEqual("four", collection[1].Value);
+            Assert.AreEqual(3, collection[2].Id);
+        }
+
+        [TestMethod]
+        public void AddRangeTest()
+        {
+            ObservableCollectionExt<TestClass> collection = new ObservableCollectionExt<TestClass>();
+
+            var obj1 = new TestClass { Id = 1, Value = "one" };
+            var obj2 = new TestClass { Id = 2, Value = "two" };
+            var obj3 = new TestClass { Id = 3, Value = "three" };
+            var obj4 = new TestClass { Id = 4, Value = "four" };
+
+            int counter = 0;
+            collection.CollectionChanged += (s, e) => counter++;
+
+            collection.AddRange(new List<TestClass> { obj1, obj2, obj3, obj4 });
+
+            Assert.AreEqual(1, counter);
+            Assert.AreEqual(4, collection.Count);
+        }
+
+        [TestMethod]
+        public void RemoveRangeTest()
+        {
+            ObservableCollectionExt<TestClass> collection = new ObservableCollectionExt<TestClass>();
+
+            var obj1 = new TestClass { Id = 1, Value = "one" };
+            var obj2 = new TestClass { Id = 2, Value = "two" };
+            var obj3 = new TestClass { Id = 3, Value = "three" };
+            var obj4 = new TestClass { Id = 4, Value = "four" };
+            collection.AddRange(new List<TestClass> { obj1, obj2, obj3, obj4 });
+
+            int counter = 0;
+            collection.CollectionChanged += (s, e) => counter++;
+
+            collection.RemoveItems(new List<TestClass> { obj2, obj4 });
+
+            Assert.AreEqual(1, counter);
+            Assert.AreEqual(2, collection.Count);
+            Assert.AreEqual(1, collection[0].Id);
+            Assert.AreEqual(3, collection[1].Id);
+        }
+    }
+}
---
+++ WPFCommandAggregator/Collections/ObservableCollectionExt.cs
@@ -0,0 +1,91 @@
+﻿namespace System.Collections.ObjectModel
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+    using System.Linq;
+
+    public class ObservableCollectionExt<T> : ObservableCollection<T>
+    {
+        public ObservableCollectionExt()
+        {
+        }
+
+        public ObservableCollectionExt(List<T> data) : base(data)
+        {
+        }
+
+        public ObservableCollectionExt(IEnumerable<T> data) : base(data)
+        {           
+        }
+
+        public void Replace(T oldItem, T newItem)
+        {
+            this.CheckReentrancy();
+
+            int foundIndex = this.Items.IndexOf(oldItem);
+            if (foundIndex >= 0)
+            {
+                this.Items[foundIndex] = newItem;
+
+                // Raise relevant notifications after all items are added
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItem, oldItem));
+                this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Items)));
+            }
+        }
+
+        /// <summary>
+        /// Adds multiple items without nofiy after each single item.
+        /// (better performance than loops with single Add calls).
+        /// </summary>
+        /// <param name="itemsToAdd">The items to add.</param>
+        public void AddRange(IEnumerable<T> itemsToAdd)
+        {
+            this.CheckReentrancy();
+            
+            if (itemsToAdd == null || !itemsToAdd.Any())
+            {
+                return;
+            }
+
+            int countBeforeAdding = this.Count;
+
+            foreach (var item in itemsToAdd)
+            {
+                this.Items.Add(item);
+            }
+
+            // Raise relevant notifications after all items are added
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, new List<T>(itemsToAdd), countBeforeAdding));
+            this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Count)));
+            this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Items)));
+        }
+
+        /// <summary>
+        /// Remove multiple items without notify after each single item.
+        /// (better performance than loops with single Remove calls).
+        /// </summary>
+        /// <param name="itemsToRemove">The items to remove.</param>
+        public void RemoveItems(IEnumerable<T> itemsToRemove)
+        {
+            this.CheckReentrancy();
+
+            if (itemsToRemove == null || !itemsToRemove.Any())
+            {
+                return;
+            }
+
+            int countBeforeRemoving = this.Count;
+
+            foreach (var item in itemsToRemove)
+            {
+                this.Items.Remove(item);
+            }
+
+            // Raise relevant notifications after all items are removed
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, new List<T>(), itemsToRemove));
+            this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Count)));
+            this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Items)));
+        }        
+    }
+}
