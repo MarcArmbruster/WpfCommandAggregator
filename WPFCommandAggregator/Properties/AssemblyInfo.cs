5e3cf60b515e3009467ccb6e81e65300638a2e67
Marc Armbruster <marcarmbruster@hotmail.de>
2020-05-23 15:15:27 +0200

    NuGet Package Version 1.6.0

--- WPFCommandAggregator/Properties/AssemblyInfo.cs
+++ WPFCommandAggregator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration(".net 4.5.2 or higher")]
 [assembly: AssemblyCompany("Marc Armbruster")]
 [assembly: AssemblyProduct("WPFCommandAggregator")]
-[assembly: AssemblyCopyright("Copyright © Marc Armbruster 2014-2018")]
+[assembly: AssemblyCopyright("Copyright © Marc Armbruster 2014-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: InternalsVisibleTo("WPFCommandAggregator.Tests")]
 //[assembly: InternalsVisibleTo("WPFCommandAggregator.Explorables")]
 
